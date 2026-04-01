d3be06239c5e2941647a2babbe0e8027912de0aa
tracer-ics2017 <tracer@njuics.org>
2026-04-01 22:47:33 +0800

    >  run 2312632 liangjingming Linux DESKTOP-8K6O69V 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:47:33 up 11 min,  1 user,  load average: 0.34, 0.14, 0.06 271536089039dd5cc40c0a3a6a43de5ed8a306df

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,14 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern size_t get_ramdisk_size();
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  (void)as;
+  (void)filename;
+
+  size_t size = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY, 0, size);
   return (uintptr_t)DEFAULT_ENTRY;
 }
