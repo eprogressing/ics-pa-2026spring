f7b5f3082b62c7909a77b0bf98672673e7b9c715
tracer-ics2017 <tracer@njuics.org>
2026-04-08 23:44:22 +0800

    >  run 2312632 liangjingming Linux DESKTOP-8K6O69V 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:44:22 up  1:08,  1 user,  load average: 0.02, 0.06, 0.07 e3491431f895dcfcc8f110f9a768d6d221abab78

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -11,7 +11,7 @@ void init_irq(void);
 void init_fs(void);
 uint32_t loader(_Protect *, const char *);
 
-static const char *init_program = "/bin/bmptest";
+static const char *init_program = "/bin/events";
 
 int main() {
 #ifdef HAS_PTE
