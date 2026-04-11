269453a8dab15e998f9cc17dd339952698ef2cc8
tracer-ics2017 <tracer@njuics.org>
2026-04-12 00:57:54 +0800

    >  run 2312632 liangjingming Linux DESKTOP-8K6O69V 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:57:54 up 28 min,  1 user,  load average: 2.40, 1.11, 0.73 f445031ef833d05748038140cdaae58d98acf1ed

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -11,7 +11,7 @@ void init_irq(void);
 void init_fs(void);
 uint32_t loader(_Protect *, const char *);
 
-static const char *init_program = "/bin/text";
+static const char *init_program = "/bin/pal";
 
 int main() {
 #ifdef HAS_PTE
