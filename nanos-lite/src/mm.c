5a1625e15532c96a3930b36a4c0ce5992dcaa196
tracer-ics2017 <tracer@njuics.org>
2026-04-20 00:08:40 +0800

    >  compile 2312632 liangjingming Linux DESKTOP-8K6O69V 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:08:40 up 47 min,  1 user,  load average: 0.13, 0.12, 0.64 1864129b6545fb8d5c8aab59ea19673215da8f44

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -4,14 +4,18 @@
 static void *pf = NULL;
 
 void* new_page(void) {
-  assert(pf < (void *)_heap.end);
+  assert(pf != NULL);
+  assert(((uintptr_t)pf & PGMASK) == 0);
+  assert((uintptr_t)pf + PGSIZE <= (uintptr_t)_heap.end);
   void *p = pf;
-  pf += PGSIZE;
+  pf = (void *)((uintptr_t)pf + PGSIZE);
+  memset(p, 0, PGSIZE);
   return p;
 }
 
 void free_page(void *p) {
-  panic("not implement yet");
+  assert(p != NULL);
+  assert(((uintptr_t)p & PGMASK) == 0);
 }
 
 /* The brk() system call handler. */
@@ -21,6 +25,8 @@ int mm_brk(uint32_t new_brk) {
 
 void init_mm() {
   pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
+  assert(((uintptr_t)pf & PGMASK) == 0);
+  assert((uintptr_t)pf <= (uintptr_t)_heap.end);
   Log("free physical pages starting from %p", pf);
 
   _pte_init(new_page, free_page);
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,6 +12,37 @@ uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
+static inline bool is_page_enabled() {
+  return cpu.cr0.paging;
+}
+
+static paddr_t page_translate(vaddr_t addr, bool is_write) {
+  uint32_t dir_idx = addr >> 22;
+  uint32_t tab_idx = (addr >> 12) & 0x3ff;
+  uint32_t page_off = addr & PAGE_MASK;
+
+  paddr_t pdir_base = cpu.cr3.page_directory_base << 12;
+  paddr_t pde_addr = pdir_base + dir_idx * sizeof(PDE);
+  PDE pde;
+  pde.val = paddr_read(pde_addr, 4);
+  assert(pde.present);
+  pde.accessed = 1;
+  paddr_write(pde_addr, 4, pde.val);
+
+  paddr_t ptab_base = pde.page_frame << 12;
+  paddr_t pte_addr = ptab_base + tab_idx * sizeof(PTE);
+  PTE pte;
+  pte.val = paddr_read(pte_addr, 4);
+  assert(pte.present);
+  pte.accessed = 1;
+  if (is_write) {
+    pte.dirty = 1;
+  }
+  paddr_write(pte_addr, 4, pte.val);
+
+  return (pte.page_frame << 12) | page_off;
+}
+
 uint32_t paddr_read(paddr_t addr, int len) {
   int map_NO = is_mmio(addr);
   if (map_NO != -1) {
@@ -30,9 +61,18 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
+  if (is_page_enabled()) {
+    assert(((addr & PAGE_MASK) + len - 1) < PAGE_SIZE);
+    return paddr_read(page_translate(addr, false), len);
+  }
   return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
+  if (is_page_enabled()) {
+    assert(((addr & PAGE_MASK) + len - 1) < PAGE_SIZE);
+    paddr_write(page_translate(addr, true), len, data);
+    return;
+  }
   paddr_write(addr, len, data);
 }
